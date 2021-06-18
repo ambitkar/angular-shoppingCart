12bc21ff9ce2552a5c3f2340b3b07fb152f7fef6
ambitkar <ambitkar4@gmail.com>
2021-06-18 21:34:22 +0530

    formArray Added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from '@angular/router';
 
 @Component({
@@ -25,8 +25,33 @@ export class RegisterComponent implements OnInit {
       phone: ['', Validators.compose([Validators.required,
       Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$"),
       Validators.minLength(10),])],
+      address: this.formBuilder.array([
+        this.getAdrresForm()
+      ]),
       password: ['', Validators.compose([Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{5,}')])],
     });
+    //console.log(this.address.controls[0].get('at')?.hasError('required'));
+    
+  }
+  getAdrresForm(): any {
+    return this.formBuilder.group({
+      at: ['', Validators.compose([Validators.required])],
+      pin: ['', [Validators.required, Validators.pattern('[0-9]{5}')]]
+    });
+  }
+
+  get address() : FormArray {
+    return this.registrationForm.get("address") as FormArray
+  }
+
+  addAddressRow(){
+    this.address.push(this.getAdrresForm());
+    console.log(this.address)
+  }
+  rmvAddressRow(i:any){
+    // const control = <FormArray>this.registrationForm.controls['address'];
+    // control.removeAt(i);
+    this.address.removeAt(i);
   }
 
   onSubmit() {
